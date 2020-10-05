4b8206a32c32777de369510925427387dc1d4c78
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-05 01:50:21 -0300

    Função Volume criada

--- Box.cpp
+++ Box.cpp
@@ -6,3 +6,10 @@ Box::Box(){
 Box::Box(double X, double Y, double Z){
     Ponto3D::set(X,Y,Z);
 }
+double Box::Area(){
+    double X = get_x(),Y = get_y(),Z = get_z();
+    return (2*X*Y)+(2*X*Z)+(2*Y*Z);
+}
+double Box::Volume(){
+    return get_x()*get_y()*get_z();
+}
