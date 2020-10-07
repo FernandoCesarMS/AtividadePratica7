a9b0b638a40c9e0da33ed1a6eb6cf5fb7cbca18e
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-10-07 18:50:46 -0300

    Parte 3 terminada, ainda não testada

--- BusinessTraveler.hpp
+++ BusinessTraveler.hpp
@@ -1,8 +1,14 @@
 #include "Pager.hpp"
 #include "Traveler.hpp"
-
+#include <iostream>
 class BusinessTraveler : public Traveler
 {
-private:
+protected:
+    Pager p = *new Pager("");
+
 public:
+    BusinessTraveler();
+    BusinessTraveler(string);
+    BusinessTraveler(const BusinessTraveler &);
+    friend istream &operator>>(istream &, BusinessTraveler &);
 };
