e446d4fca340e30d38d061386d335e4c282d9e79
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-10-07 18:51:22 -0300

    Parte 3 terminada, não testada

--- Pager.cpp
+++ Pager.cpp
@@ -1,1 +1,16 @@
 #include "Pager.hpp"
+
+Pager::Pager(string x)
+{
+    sP = x;
+}
+
+Pager::Pager(const Pager &that)
+{
+    sP = that.sP;
+}
+istream &operator>>(istream &op, Pager &that)
+{
+    op >> that.sP;
+    return op;
+}
