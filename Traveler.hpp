6b89efc5e54e48d04bc57a65f6490b8cb7b79943
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-10-07 18:53:22 -0300

    Parte 3 terminada, ainda não testada

--- Traveler.hpp
+++ Traveler.hpp
@@ -1,5 +1,14 @@
+#include <string.h>
+#include <string>
+#include "Pager.hpp"
+using namespace std;
 class Traveler
 {
-private:
+protected:
+    string sT = "";
+
 public:
+    Traveler(string);
+    Traveler(const Traveler &);
+    friend istream &operator>>(istream &, Traveler &);
 };
