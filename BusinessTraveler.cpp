076653caea51cf14c707929d4fdf98185047b6c6
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-10-07 18:48:50 -0300

    Parte 3 terminada, não testada

--- BusinessTraveler.cpp
+++ BusinessTraveler.cpp
@@ -1,1 +1,26 @@
 #include "BusinessTraveler.hpp"
+
+BusinessTraveler::BusinessTraveler() : Traveler("1")
+{
+    Pager x = *new Pager("1");
+    p = x;
+}
+BusinessTraveler::BusinessTraveler(string c) : Traveler(c)
+{
+    Pager x = *new Pager(c);
+}
+BusinessTraveler::BusinessTraveler(const BusinessTraveler &that) : Traveler("")
+{
+    sT = that.sT;
+    p = that.p;
+}
+istream &operator>>(istream &op, BusinessTraveler &that)
+{
+    cout << "Insira a string da classe Traveler: ";
+    op >> that.sT;
+    cout << endl << "Insira a string da classe Pager: ";
+    string x;
+    op >> x; // Não sei se desse jeito funciona
+    that.p = *new Pager(x);
+    return op;
+}
